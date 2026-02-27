51819da09e436787a3c8e55fac57128aa682c419
Keerthi Suresh <keerthisureshd@gmail.com>
2026-02-27 16:16:46 +0530

    UC2: Print a Hardcoded Palindrome Result

--- PalindromeChecker/src/PalindromeChecker.java
+++ PalindromeChecker/src/PalindromeChecker.java
@@ -1,10 +1,28 @@
+import java.util.Scanner;
+
 public class PalindromeChecker {
     /*
      * @author developer
      * @version 1.0
      */
     public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("version : 1.0");
+        Scanner scn = new Scanner(System.in);
+        boolean flag = true;
+        System.out.print("Input text: ");
+        String s = scn.next();
+        int n = s.length();
+        for(int i=0; i<=n/2 ;i++){
+            /*
+              checks 0 and (n-1)th char (since indexed 0)
+              so on... until middle of string
+             */
+            if(s.charAt(i)!=s.charAt(n-1-i)){
+                //if condition false flag down
+            flag = false;
+            break;
+            }
+        }
+        System.out.print("Is it a Palindrome? : "+flag);
+
     }
 }
