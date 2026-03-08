c0c5d45edf60440408d5fe686395dfd0e7f762be
Keerthi Suresh <keerthisureshd@gmail.com>
2026-03-08 09:45:08 +0530

    UC8: Linked List Based Palindrome Checker

--- PalindromeChecker/src/PalindromeChecker.java
+++ PalindromeChecker/src/PalindromeChecker.java
@@ -1,32 +1,38 @@
 import java.util.Scanner;
-import java.util.Deque;
-import java.util.ArrayDeque;
-
+import java.util.LinkedList;
+import java.util.Iterator;
 
 public class PalindromeChecker {
     /*
      * @author developer
-     * @version 7.0
+     * @version 8.0
      */
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         boolean flag = true;
         System.out.print("Input text: ");
         String s = scn.next();
-        Deque<Character> deque = new ArrayDeque<>();
+        LinkedList<Character> list = new LinkedList<>();
 
         char[] str = s.toCharArray();
 
         for(char c: str){
-            deque.add(c);
+            list.add(c);
         }
 
-        while(deque.size()>1){
-            if(deque.removeFirst()!=deque.removeLast()){
+        int n = list.size();
+
+        Iterator<Character> forward = list.iterator();
+        Iterator<Character> backward = list.descendingIterator();
+
+
+        for(int i=0;i<n/2;i++){
+            if(!forward.next().equals(backward.next())){
                 flag = false;
                 break;
             }
         }
+
 
         System.out.print("Is it a Palindrome? : ");
         System.out.println(flag);
