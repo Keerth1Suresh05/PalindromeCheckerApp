9125fe2ce2f118b9a5a74295600c0a56d6acb9d9
Keerthi Suresh <keerthisureshd@gmail.com>
2026-03-08 08:52:38 +0530

    UC6: Queue + Stack Based Palindrome Check

--- PalindromeChecker/src/PalindromeChecker.java
+++ PalindromeChecker/src/PalindromeChecker.java
@@ -1,29 +1,39 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+//import java.util.PriorityQueue;
+//can't use priority queue as it'll return alphabet priority
 
 public class PalindromeChecker {
     /*
      * @author developer
-     * @version 5.0
+     * @version 6.0
      */
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        //boolean flag = true;
+        boolean flag = true;
         System.out.print("Input text: ");
         String s = scn.next();
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
         char[] str = s.toCharArray();
 
         for(char c: str){
             stack.push(c);
+            queue.offer(c);
         }
-        String str1="";
-        while(!stack.empty()){
-            str1+=stack.pop();
+
+        while(!queue.isEmpty()){
+            if(queue.poll()!=stack.pop()){
+                flag = false;
+                break;
+            }
         }
 
         System.out.print("Is it a Palindrome? : ");
-        System.out.println(s.equals(str1));
+        System.out.println(flag);
     }
 }
 
