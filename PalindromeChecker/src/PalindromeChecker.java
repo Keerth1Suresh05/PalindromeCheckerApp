c076b418c592772031c7cb9a7ed86a35c8b1d590
Keerthi Suresh <keerthisureshd@gmail.com>
2026-03-09 13:58:39 +0530

    UC9: Recursive Palindrome Checker

--- PalindromeChecker/src/PalindromeChecker.java
+++ PalindromeChecker/src/PalindromeChecker.java
@@ -1,41 +1,30 @@
 import java.util.Scanner;
-import java.util.LinkedList;
-import java.util.Iterator;
 
 public class PalindromeChecker {
     /*
      * @author developer
      * @version 8.0
      */
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        boolean flag = true;
+        //boolean flag = true;
         System.out.print("Input text: ");
         String s = scn.next();
-        LinkedList<Character> list = new LinkedList<>();
 
-        char[] str = s.toCharArray();
-
-        for(char c: str){
-            list.add(c);
-        }
-
-        int n = list.size();
-
-        Iterator<Character> forward = list.iterator();
-        Iterator<Character> backward = list.descendingIterator();
-
-
-        for(int i=0;i<n/2;i++){
-            if(!forward.next().equals(backward.next())){
-                flag = false;
-                break;
-            }
-        }
-
+        //char[] str = s.toCharArray();
 
         System.out.print("Is it a Palindrome? : ");
-        System.out.println(flag);
+        System.out.println(check(s,0,s.length()-1));
+        //System.out.println(flag);
     }
+
+    public static boolean check(String s,int left,int right){
+        if(left>=right)return true;
+        if(s.charAt(left)!=s.charAt(right)) return false;
+        return check(s,left+1,right-1);
+    }
+
 }
+
 
